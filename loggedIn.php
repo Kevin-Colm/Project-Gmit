f96fe2e06554080fe8401d099af307174211f915
Colm <Colm@WINDOWS-3H2KHSP>
2018-02-14 12:57:01 +0000

    Added loggedIn page for testiing

---
+++ loggedIn.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div>
+        <h1>Logged in successfully</h1>
+    </div>
+</body>
+</html>
+
+
