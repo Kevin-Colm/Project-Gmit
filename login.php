8885badea330fc1ee371f1bff0d045d2e5347443
Colm <Colm@WINDOWS-3H2KHSP>
2018-02-14 14:27:40 +0000

    Cleaned up login.php

--- login.php
+++ login.php
@@ -1,37 +1,43 @@
 <?php
 //Adapted from http://codingcyber.org/simple-login-script-php-and-mysql-64/
 //https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-session_start();
- require('dbConnect.php');
-//3. If the form is submitted or not.
-//3.1 If the form is submitted
-if (isset($_POST['username']) and isset($_POST['password'])){
-//3.1.1 Assigning posted values to variables.
-$username = $_POST['username'];
-$password = $_POST['password'];
-//3.1.2 Checking the values are existing in the database or not
-$query = "SELECT username, password FROM `users` WHERE username='$username' and password='$password'";
- 
-$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-$count = mysqli_num_rows($result);
-//3.1.2 If the posted values are equal to the database values, then session will be created for the user.
-if ($count == 1){
-$_SESSION['username'] = $username;
-}else{
-//3.1.3 If the login credentials doesn't match, he will be shown with an error message.
-$fmsg = "Invalid Login Credentials.";
-}
-}
-//3.1.4 if the user is logged in Greets the user with message
-if (isset($_SESSION['username'])){
-$username = $_SESSION['username'];
-//adapted from https://stackoverflow.com/questions/20649921/php-redirect-on-login
-header("Location: loggedIn.php");
-      exit;
-echo "This is the Members Area";
-echo "<a href='logout.php'>Logout</a>";
-}
- ?>
+
+    session_start();
+     require('dbConnect.php');
+
+    //3. If the form is submitted or not.
+    //3.1 If the form is submitted
+
+    if (isset($_POST['username']) and isset($_POST['password'])){
+        //3.1.1 Assigning posted values to variables.
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        //3.1.2 Checking the values are existing in the database or not
+        $query = "SELECT username, password FROM `users` WHERE username='$username' and password='$password'";
+
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        $count = mysqli_num_rows($result);
+        //3.1.2 If the posted values are equal to the database values, then session will be created for the user.
+        if ($count == 1){
+            $_SESSION['username'] = $username;
+            }else{
+                //3.1.3 If the login credentials doesn't match, he will be shown with an error message.
+                $fmsg = "Invalid Login Credentials.";
+        }
+    }
+    
+    //3.1.4 if the user is logged in Greets the user with message
+    if (isset($_SESSION['username'])){
+        $username = $_SESSION['username'];
+        //adapted from https://stackoverflow.com/questions/20649921/php-redirect-on-login
+        header("Location: loggedIn.php");
+              exit;
+        echo "This is the Members Area";
+        echo "<a href='logout.php'>Logout</a>";
+    }
+     ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
