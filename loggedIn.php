909c2a1919d478c786855b35b80705d2a7ac58c6
Kevin <Kevin@DESKTOP-12A507O>
2018-02-15 16:24:51 +0000

    Login page changed with redirect to logged in page. Error message added if wrong password or username is entered. Logout.php file added.
    Test including ajax page into logged in page.

--- loggedIn.php
+++ loggedIn.php
@@ -1,19 +1,33 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
-<body>
-    <div>
-        <h1>Logged in successfully</h1>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+        <style type="text/css">
+            body{ font: 14px sans-serif; text-align: center; }
+        </style>
+    </head>
+    <body>
+        <div>
+
+            <h1>Logged in successfully</h1>
+            <?php
+                session_start();
+                if (isset($_SESSION['username'])) {
+                    $username = $_SESSION['username'];
+
+                    //adapted from https://stackoverflow.com/questions/20649921/php-redirect-on-login
+                    echo '<h1>Hello ' . $username . '</h1>';
+
+                    echo "<h2>This is the Members Area</h2>";
+                    include 'index.php';
+                    echo "<a href='logout.php'>Logout</a>";
+                }
+            ?>
+        </div>
+    </body>
 </html>
 
 
--- login.php
+++ login.php
@@ -1,59 +1,53 @@
 <?php
 //Adapted from http://codingcyber.org/simple-login-script-php-and-mysql-64/
-//https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
+//             https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
 session_start();
- require('dbConnect.php');
+require('dbConnect.php');
 //3. If the form is submitted or not.
 //3.1 If the form is submitted
-if (isset($_POST['username']) and isset($_POST['password'])){
-//3.1.1 Assigning posted values to variables.
-$username = $_POST['username'];
-$password = $_POST['password'];
-//3.1.2 Checking the values are existing in the database or not
-$query = "SELECT username, password FROM `users` WHERE username='$username' and password='$password'";
- 
-$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-$count = mysqli_num_rows($result);
-//3.1.2 If the posted values are equal to the database values, then session will be created for the user.
-if ($count == 1){
-$_SESSION['username'] = $username;
-}else{
-//3.1.3 If the login credentials doesn't match, he will be shown with an error message.
-$fmsg = "Invalid Login Credentials.";
+if (isset($_POST['username']) and isset($_POST['password'])) {
+    //3.1.1 Assigning posted values to variables.
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    //3.1.2 Checking the values are existing in the database or not
+    $query = "SELECT username, password FROM `users` WHERE username='$username' and password='$password'";
+
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+    $count = mysqli_num_rows($result);
+    echo $count;
+    //3.1.2 If the posted values are equal to the database values, then session will be created for the user.
+    if ($count == 1) {
+        $_SESSION['username'] = $username;
+
+        header("Location: loggedIn.php");
+    } else {
+        //3.1.3 If the login credentials doesn't match, he will be shown with an error message.
+        echo "<h1 style='color:Red;'>Invalid Username or Password.</h1>";
+    }
 }
-}
-//3.1.4 if the user is logged in Greets the user with message
-if (isset($_SESSION['username'])){
-$username = $_SESSION['username'];
-//adapted from https://stackoverflow.com/questions/20649921/php-redirect-on-login
-header("Location: loggedIn.php");
-      exit;
-echo "This is the Members Area";
-echo "<a href='logout.php'>Logout</a>";
-}
- ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-         <form class="form-signin" method="POST">
-        <h2 class="form-signin-heading">Please Login</h2>
-        <div class="input-group">
-	  <span class="input-group-addon" id="basic-addon1">@</span>
-	  <input type="text" name="username" class="form-control" placeholder="Username" required>
-	</div>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-        <a class="btn btn-lg btn-primary btn-block" href="register.php">Register</a>
-      </form>   
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+        <style type="text/css">
+            body{ font: 14px sans-serif; }
+            .wrapper{ width: 350px; padding: 20px; }
+        </style>
+    </head>
+    <body>
+        <form class="form-signin" method="POST">
+            <h2 class="form-signin-heading">Please Login</h2>
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon1">@</span>
+                <input type="text" name="username" class="form-control" placeholder="Username" required>
+            </div>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+            <a class="btn btn-lg btn-primary btn-block" href="register.php">Register</a>
+        </form>   
+    </body>
 </html>
