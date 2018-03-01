645c1d3c131e012b46450ae4269bff177bb6ef61
Kevin <Kevin@DESKTOP-12A507O>
2018-03-01 21:04:45 +0000

    TEst images working from form upload. Link image path to database next.

--- createProfile.php
+++ createProfile.php
@@ -29,7 +29,49 @@
             $name = $_POST['name'];
             //GEt tyoe from dropdown in form
             $type = $_POST['type'];
+            $target_dir = "Images/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+            if (isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                if ($check !== false) {
+                    echo "File is an image - " . $check["mime"] . ".";
+                    $uploadOk = 1;
+                } else {
+                    echo "File is not an image.";
+                    $uploadOk = 0;
+                }
+            }
+// Check if file already exists
+            if (file_exists($target_file)) {
+                echo "Sorry, file already exists.";
+                $uploadOk = 0;
+            }
+// Check file size
+            if ($_FILES["fileToUpload"]["size"] > 500000) {
+                echo "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+// Allow certain file formats
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+            }
+// Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
+
+                    echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            }
 
             //SQL auery to check if the ID PK exists
             $query = "SELECT * FROM $type WHERE id = '$id'";
@@ -37,7 +79,7 @@
             //php function to count the matching rows in the table
             $count = mysqli_num_rows($result);
             //IF count is greater than 0 the pk exists so execute an updte rather than an insert.
-            if ($count1 > 0) {
+            if ($count > 0) {
                 $query = "UPDATE $type SET name = '$name' WHERE id=$id;";
 
                 mysqli_query($conn, $query) or die(mysqli_error($conn));
@@ -62,21 +104,22 @@
                 $query = "SELECT type from userType where id= '$id'";
                 $result = $conn->query($query);
                 $row = $result->fetch_assoc();
-                header("location:" . $row['type'] . ".php");
+                echo  $target_file ;
+               // header("location:" . $row['type'] . ".php");
             }
         }
         ?>                
 
 
 
-        <form action="upload.php" method="post" enctype = "multipart/form-data">
+        <form action="" method="post" enctype = "multipart/form-data">
             <select name="type">
                 <option value="customer">Customer</option>
                 <option value="band">Band</option>
                 <option value="venue">Venue</option>
             </select>
             <input type="text" name="name" Value="" placeholder="Enter name">
-<input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" name="submit" value="Submit Profile." />
 
         </form>
