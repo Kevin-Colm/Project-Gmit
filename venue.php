0a44503f1521545cfe479d35bb08c020c7384705
Kevin <Kevin@DESKTOP-12A507O>
2018-03-02 09:57:23 +0000

    venue page added

---
+++ venue.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+session_start();
+include 'dbConnect.php';
+echo'This is the customers page<br/>';
+$id = $_SESSION['id'];
+$query = "SELECT * FROM `venue` WHERE id = '$id';";
+
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+    
+   
+   
+        $row = $result->fetch_assoc();
+          $name = $row['name'];
+          $image = $row['image'];
+    
+echo $name.'<br/>';
+ echo'<img src="'.$row["image"].'"width="200px"/>';
