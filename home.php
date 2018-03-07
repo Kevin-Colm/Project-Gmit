aeee1588f431dc5775ea7dc459d8bfb7e1f956da
Kevin <Kevin@DESKTOP-12A507O>
2018-03-07 23:23:51 +0000

    home page added with dynamic created elements from database

---
+++ home.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'partials/header.php';
+include 'partials/home_hero.php';
+include 'partials/giglist.php';
+include 'partials/footer.php';
---
+++ partials/gigList.php
@@ -0,0 +1,32 @@
+<?php
+include'dbConnect.php';
+
+ $query = "SELECT * FROM `event`";
+
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        while ($row = $result->fetch_assoc()) {
+            $desc = $row['description'];
+            $date = $row['date'];
+            ?>
+           <!-- Blog Post -->
+          <div class="card mb-4">
+           
+            <div class="card-body">
+                
+              <h2 class="card-title">Post Title</h2>
+              <img class="float-right" src="http://placehold.it/200x200" alt="Card image cap">
+              <p class="card-text"><?php echo $desc ?></p>
+              
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+              
+            </div>
+              
+            <div class="card-footer text-muted">
+              Posted on <?php echo $date ?> by
+              <a href="#">Start Bootstrap</a>
+              
+            </div>
+          </div>
+           
+      <?php  }?>
+</div>
