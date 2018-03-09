aa178301a241f0bf97116e8f716bf5229ed998ec
Kevin <Kevin@DESKTOP-12A507O>
2018-03-09 19:17:51 +0000

    database exoprt updated

--- Export Database/export.sql
+++ Export Database/export.sql
@@ -40,9 +40,35 @@ CREATE TABLE IF NOT EXISTS `customer` (
   CONSTRAINT `FK_customer_users` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table gigguide.customer: ~0 rows (approximately)
+-- Dumping data for table gigguide.customer: ~1 rows (approximately)
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` (`id`, `name`, `image`) VALUES
+	(37, 'Frank Bruno', 'Images/PegSolitaire_1000.gif'),
+	(39, 'johhhny 5er', 'Images/thubmnail.png');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+
+-- Dumping structure for table gigguide.event
+CREATE TABLE IF NOT EXISTS `event` (
+  `venueId` int(11) DEFAULT NULL,
+  `rateVenue` float DEFAULT NULL,
+  `rateBand` float DEFAULT NULL,
+  `review` varchar(2000) DEFAULT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `bandId` int(11) DEFAULT NULL,
+  KEY `FK_event_venue` (`venueId`),
+  KEY `FK_event_band` (`bandId`),
+  CONSTRAINT `FK_event_band` FOREIGN KEY (`bandId`) REFERENCES `band` (`id`),
+  CONSTRAINT `FK_event_venue` FOREIGN KEY (`venueId`) REFERENCES `venue` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table gigguide.event: ~3 rows (approximately)
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+INSERT INTO `event` (`venueId`, `rateVenue`, `rateBand`, `review`, `description`, `date`, `bandId`) VALUES
+	(36, NULL, NULL, NULL, 'this is the event description', '2018-03-15', NULL),
+	(38, NULL, NULL, NULL, 'this is the second description for the event', '2018-03-21', NULL),
+	(40, NULL, NULL, NULL, 'This is the third entry that should display on the hoame page list.', '2018-03-30', NULL);
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
 
 -- Dumping structure for table gigguide.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -51,10 +77,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `userPassword` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
 
--- Dumping data for table gigguide.users: ~0 rows (approximately)
+-- Dumping data for table gigguide.users: ~5 rows (approximately)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `userPassword`) VALUES
+	(36, 'kevin', '1111'),
+	(37, 'frank', '1111'),
+	(38, 'dave', '1111'),
+	(39, 'jhonny5', '1111'),
+	(40, 'harold', '1111');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Dumping structure for table gigguide.usertype
@@ -65,8 +97,14 @@ CREATE TABLE IF NOT EXISTS `usertype` (
   CONSTRAINT `FK_usertype_users` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table gigguide.usertype: ~0 rows (approximately)
+-- Dumping data for table gigguide.usertype: ~5 rows (approximately)
 /*!40000 ALTER TABLE `usertype` DISABLE KEYS */;
+INSERT INTO `usertype` (`id`, `type`) VALUES
+	(36, 'venue'),
+	(37, 'customer'),
+	(38, 'venue'),
+	(39, 'customer'),
+	(40, 'venue');
 /*!40000 ALTER TABLE `usertype` ENABLE KEYS */;
 
 -- Dumping structure for table gigguide.venue
@@ -78,11 +116,15 @@ CREATE TABLE IF NOT EXISTS `venue` (
   `description` varchar(2000) DEFAULT NULL,
   `rating` float DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `FK_venue_users` FOREIGN KEY (`id`) REFERENCES `users` (`id`)
+  CONSTRAINT `FK_venue_users` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table gigguide.venue: ~0 rows (approximately)
+-- Dumping data for table gigguide.venue: ~1 rows (approximately)
 /*!40000 ALTER TABLE `venue` DISABLE KEYS */;
+INSERT INTO `venue` (`id`, `name`, `address`, `image`, `description`, `rating`) VALUES
+	(36, 'kevin the 3rd', NULL, 'Images/PegSolitaire_1000.gif', NULL, NULL),
+	(38, 'dave rave', NULL, 'Images/PegSolitaire_1000.gif', NULL, NULL),
+	(40, NULL, NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `venue` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
