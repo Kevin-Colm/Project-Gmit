3111d918e14edd9b3d548b4ef23c7e5d2c74d3d6
Kevin <Kevin@DESKTOP-12A507O>
2018-03-14 14:08:02 +0000

    new form added to register page.
    Form elements need to be customized for the register function.
    Can adapt the test form.

--- register.php
+++ register.php
@@ -40,18 +40,7 @@ if (isset($_POST['userName']) and isset($_POST['password'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Register</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-        <style type="text/css">
-            body{ font: 14px sans-serif; }
-            .wrapper{ width: 350px; padding: 20px; }
-        </style>
-    </head>
-    <body>
+
         <form class="form-signin" method="POST">
             
             <h2 class="form-signin-heading">Please Register here</h2>
@@ -72,6 +61,60 @@ if (isset($_POST['userName']) and isset($_POST['password'])) {
             <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
             <!--            <a class="btn btn-lg btn-primary btn-block" href="register.php">Register</a>-->
         </form>  
-        <a href='logout.php'>Logout</a></br>
-    </body>
-</html>
+<div id="login-overlay" class="modal-dialog modal-lg">
+        
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                    <h2 class="modal-title" id="myModalLabel">GigGuide</h2>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="well">
+                                <form id="loginForm" method="POST"  novalidate="novalidate">
+                                    <div class="form-group">
+                                        <label for="username" class="control-label">Username</label>
+                                        <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter you username" placeholder="e.g. joebloggs">
+                                        <span class="help-block"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlSelect1">Example select</label>
+                                        <select  class="form-control form-control-lg" id="exampleFormControlSelect1">
+                                          <option value="customer">Customer</option>
+                <option value="band">Band</option>
+                <option value="venue">Venue</option>
+                                        </select>
+                                     </div>
+                                    <div class="form-group">
+                                        <label for="password" class="control-label">Password</label>
+                                        <input type="password" class="form-control" id="password" name="password" value="" required="" title="Please enter your password">
+                                        <span class="help-block"></span>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember" id="remember"> Remember login
+                                        </label>
+                                        <p class="help-block">(if this is a private computer)</p>
+                                    </div>
+                                    <button type="submit" class="btn btn-success btn-block">Login</button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <p class="lead">Don't have an account?<span class="text-success">Register here!</span></p>
+                            <ul class="list-unstyled" style="line-height: 2">
+                                <li><span class="fa fa-check text-success"></span> See the latest gigs in your area</li>
+                                <li><span class="fa fa-check text-success"></span> Promote your venue</li>
+                                <li><span class="fa fa-check text-success"></span> Advertise your band</li>
+                                <li><span class="fa fa-check text-success"></span> Easy to use</li>
+								
+                            </ul>
+                            <p><a href="register.php" class="btn btn-info btn-block">Register now!</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+  <?php
+  include 'partials/footer.php';
