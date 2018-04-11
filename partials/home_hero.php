d7a15a61da23504e91856c1caa7a3d37da8b6e0e
Colm <Colm@WINDOWS-3H2KHSP>
2018-04-11 12:30:10 +0100

    Added the site logo to the jumbotron in the home hero partial

--- partials/home_hero.php
+++ partials/home_hero.php
@@ -1,11 +1,10 @@
 
 
 <div class="jumbotron">
-  <h1 class="display-3">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <a><img src="./Images/Banner.png" height="200"alt=""/></a>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p>GigGuide is a website where you can find the best events happening around you. We aim to provide the best user, venue and band experience.</p>
   <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Register Here!</a>
   </p>
 </div>
