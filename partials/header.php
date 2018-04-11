3a36e8698c00de92a3ee31149f22a8b29e7466ec
Colm <Colm@WINDOWS-3H2KHSP>
2018-04-11 12:42:10 +0100

    Added the logo to the header bar with a link back to the home page

--- partials/header.php
+++ partials/header.php
@@ -41,7 +41,7 @@ include 'dbConnect.php';
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a href="home.php"><img src="./Images/GigGuide Logo.png" height="50" alt=""/></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
