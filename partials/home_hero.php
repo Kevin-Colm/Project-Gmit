13b20953de3e28ed5c6c8c8a3df6785724d3cee2
Colm <Colm@WINDOWS-3H2KHSP>
2018-04-11 12:35:09 +0100

    Added the register now button with a link to the register page

--- partials/home_hero.php
+++ partials/home_hero.php
@@ -5,6 +5,6 @@
   <hr class="my-4">
   <p>GigGuide is a website where you can find the best events happening around you. We aim to provide the best user, venue and band experience.</p>
   <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Register Here!</a>
+    <a class="btn btn-primary btn-lg" href=" register.php" role="button">Register Here!</a> <!-- This will link to the register page -->
   </p>
 </div>
