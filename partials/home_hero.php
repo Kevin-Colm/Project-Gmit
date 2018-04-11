f8c0008668dfc2e9e4db7839c7d08a9f6cc76910
Colm <Colm@WINDOWS-3H2KHSP>
2018-04-11 12:45:18 +0100

    Added the logo to the header bar with a link back to the home page

--- partials/home_hero.php
+++ partials/home_hero.php
@@ -1,7 +1,7 @@
 
 
 <div class="jumbotron">
-    <a><img src="./Images/Banner.png" height="200"alt=""/></a>
+  <a><img src="./Images/Banner.png" height="200"alt=""/></a>
   <hr class="my-4">
   <p>GigGuide is a website where you can find the best events happening around you. We aim to provide the best user, venue and band experience.</p>
   <p class="lead">
