228282999809689680912735b1780ededa4612f4
Kevin <Kevin@DESKTOP-12A507O>
2018-04-11 12:58:28 +0100

    top 3 bands added to side widget.

---
+++ partials/topThree.php
@@ -0,0 +1,31 @@
+<?php
+
+
+ $query = "select *
+from band
+order by rating desc
+limit 3";
+ 
+    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        while ($row = $result->fetch_assoc()) {
+           
+           
+            $name = $row['name'];
+           $bandId = $row['id'];
+            $image = $row['image'];
+           $rating = $row['rating'];
+           
+            
+           ?>
+
+ <a href="singleProfile.php?id=<?php echo $bandId ?>"><h2 class="card-title"><?php echo $name ?></h2></a>
+ <img class="img-fluid rounded mb-3 mb-md-0" src="<?php echo $image ?>" alt="">
+ <h5>Rating: <?php echo $rating ?></h5>
+ <span class="stars"><?php echo $rating ?></span>
+ <hr/>
+ <?php
+  
+        }
+            
+
+
