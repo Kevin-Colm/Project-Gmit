94f87355aeea27cf0aa437105fe03f58993d3b47
Kevin <Kevin@DESKTOP-12A507O>
2018-04-18 17:09:13 +0100

    Comments added

--- rating.php
+++ rating.php
@@ -15,22 +15,13 @@ $eventId = $_GET['id'];
 $query = "select * from event where id = $eventId";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $row = $result->fetch_assoc();
- 
-       
-           
-            
-  
-            
+   
+//date to only have rating for past events            
 $date = new DateTime($row['date']);
 $now = new DateTime();
 
 if($date < $now) {
     
-
-
-    
-
-        
 
     if (isset($_POST['rateBand'])) {
 
@@ -62,6 +53,7 @@ if($date < $now) {
     ?>
     <!-- Sidebar Widgets Column -->
     <!-- Side Widget -->
+    <!-- Rating system adapted from -->
     <div class="card my-4">
         <h5 class="card-header">Rate Band</h5>
         <div class="card-body">
@@ -87,29 +79,33 @@ if($date < $now) {
     </div>
     <?php
 
-
+    //Check if The rate venue form has been submitted
     if (isset($_POST['rateVenue'])) {
+        //Get the value of the radio button selected and store it in this variable.
+        $rating = $_POST['ratings']; 
 
-        $rating = $_POST['ratings'];  //  Displaying Selected Value
-
-       
+       //Getting this value from the top sql query line 15
         $venueid = $row['venueId'];
-
+        //query to check for previous votes of the same event.
         $query5 = "select userId from ratings where userId= $userId and id = $venueid and eventId = $eventId";
         $result5 = mysqli_query($conn, $query5) or die(mysqli_error($conn));
         $num_rows1 = mysqli_num_rows($result5);
-       
+       //if the number of returned rows are 1 or more the votre has already benn cast
         if ($num_rows1 > 0) {
             echo '<script>alert("You have already voted for this venue");</script>';
         } else {
+            //insert the rating to the rating table with the venueId and  user ID.
         $query = "insert into ratings(id,rating,userId,eventId) values($venueid,$rating,$userId,$eventId)";
+        
         $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        //get average rating from the table the as alias can be used to update the rating of the venue
         $query2 = "select round(avg(rating),2) as avg from ratings where id = $venueId";
 
         $result2 = mysqli_query($conn, $query2) or die(mysqli_error($conn));
         $row2 = $result2->fetch_assoc();
+        //Get average value from the ratings table.
         $avg = $row2['avg'];
-
+        //update the rating for the vunue.
         $query3 = "update venue set rating=$avg where id = $venueId";
         mysqli_query($conn, $query3) or die(mysqli_error($conn));
         }
