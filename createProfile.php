378b799cb8022ddfc66898b55116d117616dd017
Kevin <Kevin@DESKTOP-12A507O>
2018-04-26 00:25:02 +0100

    undo allowed image size increased.

--- createProfile.php
+++ createProfile.php
@@ -27,19 +27,36 @@ if (isset($_POST['submit'])) {
     $bio = mysqli_real_escape_string($conn,$_POST['bio']);
     $target_dir = "Images/";
     $address = mysqli_real_escape_string($conn,$_POST['address']);
-    $uploadOk = 1;
+    
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-   
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
     if (isset($_POST["submit"])) {
-        
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
 // Check if file already exists
     if (file_exists($target_file)) {
 
         $uploadOk = 0;
     }
-
-
+// Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+// Allow certain file formats
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
 // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
@@ -81,7 +98,6 @@ if (isset($_POST['submit'])) {
         mysqli_query($conn, $query) or die(mysqli_error($conn));
         header("location: profile.php");
     }
-}
 }
                      
 
